2474b9fa37953fa5f3d45dfb6f3b956e4ad6a617
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-12-11 21:22:09 +0530

    Update App.js
    Fixed App Name

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 
-let CounterApp = ()=> {
+let App = ()=> {
   let [greet, setGreet] = useState("");
   return (
     <div>
         <p>Enter your name:</p>
-        <input type="text" onChange ={(e)=> setGreet(e.target.value)}/>
-        {greet ? <p>Hello {greet}!</p>: ""}
+        <input type="text" onKeyUp ={(e)=> setGreet(`Hello ${e.target.value}!`)}/>
+        <p>{greet}</p>
     </div>
   )
 }
 
-export default CounterApp;
+export default App;
