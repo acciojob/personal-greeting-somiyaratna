546404b775d101610b3557327b8bb75a16b49dfe
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-12-12 21:05:06 +0530

    Final attempt
    Yet another attempt

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import './../styles/App.css';
 
-let App = ()=> {
-  let [greet, setGreet] = useState("");
+const App = () => {
+  const [input, setInput] = useState("");
+
   return (
     <div>
-        <p>Enter your name:</p>
-        <input type="text" onKeyUp ={(e)=> setGreet(`Hello ${e.target.value}!`)}/>
-        <p>{greet}</p>
+        {/* Do not remove the main div */}
+        { input ? <p>Hello {input}!</p>:<p>Enter your name:</p> }
+        <input type="text" onInput = {(event) => setInput(event.target.value)}/>
     </div>
   )
 }
 
-export default App;
+export default App
